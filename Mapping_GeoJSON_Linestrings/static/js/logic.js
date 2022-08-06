8fa82307304d185357ced50c083e5f223267c633
Claudia Martin Calderon <103139895+ClaudAMC@users.noreply.github.com>
2022-08-06 17:29:01 -0400

    commit on branch

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -5,14 +5,14 @@ console.log("working")
 
 
 // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -33,7 +33,7 @@ let map = L.map("mapid", {
         44.0,-80.0
     ],
     zoom: 2,
-    layers: [light]
+    layers: [dark]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -47,5 +47,16 @@ let torontoData = "https://raw.githubusercontent.com/ClaudAMC/Mapping_Earthquake
 d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map)
+    L.geoJSON(data, {
+        style: myStyle,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("<h3>" + "Airline: " + feature.properties.airline + "</h3>" + "<hr>" + "<p>" + "Destination: " + feature.properties.dst + "<p>")
+    }
+    }).addTo(map)
 });
+
+// Create a style for the lines.
+let myStyle = {
+    color: "yellow",
+    weight: 2
+}
